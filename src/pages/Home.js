32b5304501adc24d82a7429827c6cf966667c401
obaidurx <mdobaidurbd@gmail.com>
2022-01-19 18:32:28 +0600

    start to create my personal blog page

---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-5 text-gray-900">
+        Hello, Welcome to my Blog ☺️
+      </h1>
+      <p className="mx-auto loading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore maiores
+        impedit nisi rem voluptates inventore aperiam fuga eveniet.
+        Exercitationem nemo delectus possimus voluptate dolorum iusto numquam
+        doloribus ut libero labore. Velit tempore ea nemo architecto molestiae,
+        quisquam pariatur officiis autem exercitationem facere ex esse et porro
+        eius repellendus praesentium numquam!{" "}
+      </p>
+
+      <p className="mx-auto loading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod
+        assumenda, voluptatum quas eum recusandae et accusantium tenetur
+        eligendi, natus magnam earum exercitationem beatae aspernatur tempore
+        ducimus enim unde fugit optio provident ipsam dolor rem minima
+        laudantium! Odio eum ipsa autem dolor quasi repellat rem. Voluptates,
+        tempora. Veritatis beatae id voluptatibus porro tempore illo
+        consequatur.
+      </p>
+
+      <p className="mx-auto loading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quo,
+        cupiditate, recusandae et fugit quasi, repudiandae reprehenderit eaque
+        similique dignissimos obcaecati. Vero temporibus id at, dignissimos
+        earum exercitationem voluptatum reiciendis!{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Home;
